b53dc39cf22785821709a80b6d864dbd73d1a88b
Kalp Doshi <kalpdoshi999@gmail.com>
2024-07-11 16:02:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+'/home.html')
+})
+
+app.get('/home',(req,res)=> {
+    res.sendFile(__dirname+'/home.html')
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname+'/about.html')
+})
+
+app.get('/contact', (req,res) => {
+    res.sendFile(__dirname+'/contact.html')
+})
+app.listen(port, () => {
+  console.log(`I Love You ${port}`)
+})
